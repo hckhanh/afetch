04d252bff86bb0f256563446d580e4e00017d5f4
Botond Balazs <balazsbotond@gmail.com>
2020-09-15 22:58:54 +0200

    Add project scaffolding with configurations and workflows
    - Set up Changesets for versioning.
    - Added GitHub workflows for testing, releases, and CodeQL analysis.
    - Introduced contribution guidelines, code of conduct, and project documentation.
    - Established `.gitignore` and base configuration files for Syncpack and Knip.

---
+++ README.md
@@ -0,0 +1,131 @@
+# fast-url [![NPM Downloads](https://img.shields.io/npm/dw/fast-url)](https://www.npmjs.com/package/fast-url) [![JSR](https://jsr.io/badges/@hckhanh/fast-url/weekly-downloads)](https://jsr.io/@hckhanh/fast-url)
+
+A folk version of the [urlcat](https://github.com/balazsbotond/urlcat) focuses on performance and simplicity.
+Build correct URLs easily. A fast, minimal fork of urlcat focused on performance and simplicity.
+
+[![Test](https://github.com/hckhanh/fast-url/actions/workflows/test.yml/badge.svg)](https://github.com/hckhanh/fast-url/actions/workflows/test.yml)
+[![codecov](https://codecov.io/github/hckhanh/fast-url/graph/badge.svg?token=6W7S96H6OT)](https://codecov.io/github/hckhanh/fast-url)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=hckhanh_fast-url&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=hckhanh_fast-url)
+[![Bundle Size](https://badgen.net/bundlephobia/minzip/fast-url)](https://bundlephobia.com/result?p=fast-url)
+[![CodSpeed](https://img.shields.io/endpoint?url=https://codspeed.io/badge.json)](https://codspeed.io/hckhanh/fast-url)
+
+fast-url is a tiny JavaScript/TypeScript library that makes building URLs convenient and prevents common mistakes.
+
+## Features
+
+- Lightweight: Only one dependency (fast-querystring) and minimal bundle size
+- Type safe: Written in TypeScript with full type definitions
+- URL safe: Automatically escapes parameters and handles edge cases
+- Unicode-aware: Uses `codePointAt` for proper Unicode handling, including graceful encoding of lone surrogates
+- Flexible: Multiple ways to build URLs for different use cases
+
+## Installation
+
+```bash
+# Using JSR (recommended for Deno)
+deno add jsr:@hckhanh/fast-url
+
+# Using bun
+bun add fast-url
+
+# Using npm
+npm install fast-url
+```
+
+## Usage
+
+![Basic usage example](docs/urlcat-basic-usage.svg#gh-light-mode-only)
+![Basic usage example (dark mode)](docs/urlcat-basic-usage-dark.svg#gh-dark-mode-only)
+
+### Basic URL building
+
+```javascript
+import { createUrl } from "fast-url";
+
+// Path parameters
+createUrl("https://api.example.com", "/users/:id", { id: 123 });
+// → 'https://api.example.com/users/123'
+
+// Query parameters
+createUrl("https://api.example.com", "/users", { limit: 10, offset: 20 });
+// → 'https://api.example.com/users?limit=10&offset=20'
+
+// Combined path and query parameters
+createUrl("https://api.example.com", "/users/:id/posts", { id: 123, limit: 10 });
+// → 'https://api.example.com/users/123/posts?limit=10'
+```
+
+### CommonJS
+
+```javascript
+const { createUrl } = require("fast-url");
+```
+
+### Utility functions
+
+```javascript
+import { query, subst, join } from "fast-url";
+
+// Build query strings
+query({ name: "John", age: 30 });
+// → 'name=John&age=30'
+
+// Substitute path parameters
+subst("/users/:id/posts/:postId", { id: 123, postId: 456 });
+// → '/users/123/posts/456'
+
+// Join URL parts
+join("https://api.example.com/", "/", "/users");
+// → 'https://api.example.com/users'
+```
+
+## API
+
+### `createUrl(baseUrl, pathTemplate, params?)`
+
+Build a complete URL by combining a base URL, path template, and parameters.
+
+### `createUrl(baseTemplate, params)`
+
+Use a single template containing path parameters; unused params become query parameters.
+
+### `query(params)`
+
+Build a query string from an object of key-value pairs.
+
+### `subst(template, params)`
+
+Substitute path parameters in a template string.
+
+### `join(part1, separator, part2)`
+
+Join two URL parts with exactly one separator.
+
+## Why fast-url?
+
+Building URLs manually is error-prone:
+
+```javascript
+// ❌ Error-prone manual approach
+const url = `${baseUrl}/users/${id}/posts?limit=${limit}&offset=${offset}`;
+// Issues: double slashes, unescaped parameters, complex concatenation
+```
+
+```javascript
+// ✅ Clean and safe with fast-url
+const url = createUrl(baseUrl, "/users/:id/posts", { id, limit, offset });
+```
+
+fast-url handles:
+
+- Automatic parameter escaping
+- Proper URL segment joining
+- Clean separation of path and query parameters
+
+## Contributing
+
+Contributions are welcome! Please feel free to submit a Pull Request.
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,226 @@
+/**
+ * Fast URL builder with type safety and performance optimization.
+ *
+ * This module provides utilities to build URLs safely and efficiently,
+ * handling path parameters, query strings, and URL joining operations.
+ *
+ * @example
+ * ```ts
+ * import { createUrl } from "fast-url";
+ *
+ * // Build URL with path and query parameters
+ * createUrl('https://api.example.com', '/users/:id', { id: 42, search: 'foo' })
+ * // -> 'https://api.example.com/users/42?search=foo'
+ * ```
+ *
+ * @module
+ */
+
+import { stringify } from './querystring/stringify.ts'
+
+/**
+ * A map of parameters for URL building.
+ * Used for both path parameters and query string parameters.
+ */
+export type ParamMap = Record<string, unknown>
+
+/**
+ * Builds a URL using the base template and specified parameters.
+ *
+ * @param {String} baseTemplate a URL template that contains zero or more :params
+ * @param {Object} params an object with properties that correspond to the :params
+ *   in the base template. Unused properties become query params.
+ *
+ * @returns {String} a URL with path params substituted and query params appended
+ *
+ * @example
+ * ```ts
+ * createUrl('http://api.example.com/users/:id', { id: 42, search: 'foo' })
+ * // -> 'http://api.example.com/users/42?search=foo'
+ * ```
+ */
+export function createUrl(baseTemplate: string, params: ParamMap): string
+
+/**
+ * Concatenates the base URL and the path specified using `/` as a separator.
+ * If `/` occurs at the concatenation boundary in either parameter, it is removed.
+ *
+ * @param {String} baseUrl the first part of the URL
+ * @param {String} path the second part of the URL
+ *
+ * @returns {String} the result of the concatenation
+ *
+ * @example
+ * ```ts
+ * createUrl('http://api.example.com/', '/users')
+ * // -> 'http://api.example.com/users'
+ * ```
+ */
+export function createUrl(baseUrl: string, path: string): string
+
+/**
+ * Concatenates the base URL and the path specified using `/` as a separator.
+ * If `/` occurs at the concatenation boundary in either parameter, it is removed.
+ * Substitutes path parameters with the properties of the @see params object and appends
+ * unused properties in the path as query params.
+ *
+ * @param {String} baseUrl the first part of the URL
+ * @param {String} pathTemplate the second part of the URL
+ * @param {Object} params Object with properties that correspond to the :params
+ *   in the base template. Unused properties become query params.
+ *
+ * @returns {String} URL with path params substituted and query params appended
+ *
+ * @example
+ * ```ts
+ * createUrl('http://api.example.com/', '/users/:id', { id: 42, search: 'foo' })
+ * // -> 'http://api.example.com/users/42?search=foo'
+ * ```
+ */
+export function createUrl(
+  baseUrl: string,
+  pathTemplate: string,
+  params: ParamMap,
+): string
+
+export function createUrl(
+  baseUrlOrTemplate: string,
+  pathTemplateOrParams: string | ParamMap,
+  maybeParams: ParamMap = {},
+): string {
+  return typeof pathTemplateOrParams === 'string'
+    ? createUrlImpl(pathTemplateOrParams, maybeParams, baseUrlOrTemplate)
+    : createUrlImpl(baseUrlOrTemplate, pathTemplateOrParams, undefined)
+}
+
+function joinFullUrl(
+  renderedPath: string,
+  baseUrl: string,
+  pathAndQuery: string,
+): string {
+  return renderedPath.length
+    ? join(baseUrl, '/', pathAndQuery)
+    : join(baseUrl, '?', pathAndQuery)
+}
+
+function createUrlImpl(
+  pathTemplate: string,
+  params: ParamMap,
+  baseUrl: string | undefined,
+) {
+  const { renderedPath, remainingParams } = path(pathTemplate, params)
+  const cleanParams = removeNullOrUndef(remainingParams)
+  const renderedQuery = query(cleanParams)
+  const pathAndQuery = join(renderedPath, '?', renderedQuery)
+
+  return baseUrl
+    ? joinFullUrl(renderedPath, baseUrl, pathAndQuery)
+    : pathAndQuery
+}
+
+/**
+ * Creates a query string from the specified object.
+ *
+ * @param {Object} params an object to convert into a query string.
+ *
+ * @returns {String} Query string.
+ *
+ * @example
+ * ```ts
+ * query({ id: 42, search: 'foo' })
+ * // -> 'id=42&search=foo'
+ * ```
+ */
+export function query(params: ParamMap): string {
+  return Object.keys(params).length ? stringify(params) : ''
+}
+
+/**
+ * Substitutes :params in a template with property values of an object.
+ *
+ * @param {String} template a string that contains :params.
+ * @param {Object} params an object with keys that correspond to the params in the template.
+ *
+ * @returns {String} Rendered path after substitution.
+ *
+ * @example
+ * ```ts
+ * subst('/users/:id/posts/:postId', { id: 42, postId: 36 })
+ * // -> '/users/42/posts/36'
+ * ```
+ */
+export function subst(template: string, params: ParamMap): string {
+  const { renderedPath } = path(template, params)
+  return renderedPath
+}
+
+function path(template: string, params: ParamMap) {
+  const remainingParams = { ...params }
+  const renderedPath = template.replace(/:[_A-Za-z]+\w*/g, (p) => {
+    const key = p.slice(1)
+    validatePathParam(params, key)
+    delete remainingParams[key]
+    return encodeURIComponent(params[key] as string | number | boolean)
+  })
+  return { renderedPath, remainingParams }
+}
+
+function validatePathParam(params: ParamMap, key: string) {
+  if (!Object.hasOwn(params, key)) {
+    throw new Error(`Missing value for path parameter ${key}.`)
+  }
+  const type = typeof params[key]
+  if (type !== 'boolean' && type !== 'string' && type !== 'number') {
+    throw new TypeError(
+      `Path parameter ${key} cannot be of type ${type}. ` +
+        'Allowed types are: boolean, string, number.',
+    )
+  }
+  if (type === 'string' && (params[key] as string).trim() === '') {
+    throw new Error(`Path parameter ${key} cannot be an empty string.`)
+  }
+}
+
+/**
+ * Joins two strings using a separator.
+ * If the separator occurs at the concatenation boundary in either of the strings, it is removed.
+ * This prevents accidental duplication of the separator.
+ *
+ * @param {String} part1 First string.
+ * @param {String} separator Separator used for joining.
+ * @param {String} part2 Second string.
+ *
+ * @returns {String} Joined string.
+ *
+ * @example
+ * ```ts
+ * join('first/', '/', '/second')
+ * // -> 'first/second'
+ * ```
+ */
+export function join(part1: string, separator: string, part2: string): string {
+  const p1 = part1.endsWith(separator)
+    ? part1.slice(0, -separator.length)
+    : part1
+  const p2 = part2.startsWith(separator) ? part2.slice(separator.length) : part2
+  return !p1 || !p2 ? p1 + p2 : p1 + separator + p2
+}
+
+/**
+ * Removes null and undefined values from a parameter map.
+ * This function filters out any properties with null or undefined values,
+ * returning a new object containing only defined values.
+ *
+ * @param {ParamMap} params The parameter map to filter
+ *
+ * @example
+ * ```ts
+ * removeNullOrUndef({ a: 'hello', b: null, c: undefined, d: 'world' })
+ * // -> { a: 'hello', d: 'world' }
+ * ```
+ */
+function removeNullOrUndef<P extends ParamMap>(params: P) {
+  return Object.fromEntries(
+    Object.entries(params).filter(([, value]) => value != null),
+  ) as { [K in keyof P]: NonNullable<P[K]> }
+}
